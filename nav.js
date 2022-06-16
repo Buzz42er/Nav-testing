f1c209bc807fd1b24d70784fdcb3e1f5a1ca72c3
Buzz <jure.eres42@gmail.com>
2022-06-16 20:11:07 +0200

    problem
    first click on menu icon doesn't work
    hover animation works, before second click on menu
    after second click nav popsup like intended on click
    but then hover does not work

---
+++ nav.js
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//nav popup
+
+const navHover = function navHover() {
+    const nav = document.getElementById("nav");
+
+    nav.style.transition = "all 1s";
+  
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Menu button
+
+const openNav = function openNav() {
+  const menuButton = document.getElementById("menuButton");
+  const nav = document.getElementById("nav");
+
+  menuButton.addEventListener("click", function () {
+    nav.style.height = "50px";
+    console.log("to 50px");
+
+    setTimeout(function navBackDown() {
+      nav.style.height = "10px";
+      console.log("to 10px");
+    }, 600);
+  });
+};
+
